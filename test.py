1d8e6fb7305fab555c6d17622629b92d7971eacb
gon2gon2 <ark5354@gmail.com>
2020-10-05 23:08:29 +0900

    10.05

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 import sys
-orig_score=[]
-# n = int(input())
-# *n = map(int, sys.stdin.readlines().split())
-_,*m = map(int, sys.stdin.read().split())
+# 테스트의 개수
+C = int(input())
+for i in range(C):
+    first = 0
+    a = input().split()
+    lst = []
 
-print(n)
+    for i in a:
+        lst.append(int(i))
+    first = lst.pop(0)
+    mean = sum(lst)/first
+    cnt=0
+    for i in lst:
+        if i>mean:
+            cnt += 1
+    print(round(cnt/first*100,3),end='')
+    print('%')
